92ef13b4494e48e696fcba9e313a78a041cda00a
DanielKick-USDA <Daniel.Kick@USDA.gov>
2024-11-18 17:26:12 -0600

    Added setup script

---
+++ Virus_project_files/00_setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Unzip data files discarding non-data directory
+unzip virus_project_data_files.zip
+rm -r __MACOSX/
+# create directory for exteranal data
+mkdir ext_data
+mv Drosophila_melanogaster  Glycine_max  Spodoptera_frugiperda  Vitis_vinifera  Zea_mays ./ext_data/
+
+# create directory for processed data
+mkdir data
